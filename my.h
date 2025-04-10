6960dd7e679dab551f1d7b13432956fb5741bf54
Ani712 <sharmapremlata244@gmail.com>
2025-04-10 23:40:10 +0530

    Update and rename .gitignore to my.h

---
+++ my.h
@@ -0,0 +1,68 @@
+// Name = Ani Sharma
+// Roll no. 2415000214(10)
+// Section - AG -1
+
+// basics.h
+#ifndef MY_H
+#define MY_H
+
+// Function to get integer input from user
+int get_input()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    return num;
+}
+
+// Reverse a number
+int reverse_number(int num)
+{
+    int reversed = 0;
+    while(num != 0)
+    {
+        reversed = reversed * 10 + num % 10;
+        num = num / 10;
+    }
+    return reversed;
+}
+
+
+int is_strong_number(int num)
+{
+    int original = num;
+    int sum = 0;
+    
+    while(num != 0)
+    {
+        int digit = num % 10;
+        int fact = 1;
+        
+        
+        for(int i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+        
+        sum = sum + fact;
+        num = num / 10;
+    }
+    
+    if(sum == original)
+        return 1; 
+    else
+        return 0; 
+}
+
+
+int factorial(int num)
+{
+    int result = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+#endif
